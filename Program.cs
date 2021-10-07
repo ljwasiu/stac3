095fe8255e7b3657d465e0e6df2f7638719a0065
ljwasiu <92114733+ljwasiu@users.noreply.github.com>
2021-10-07 19:56:58 +0200

    .pa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stac3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
